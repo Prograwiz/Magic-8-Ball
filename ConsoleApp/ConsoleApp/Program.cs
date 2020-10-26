c3ab7b3787054b16f0bffcedfdb6bae6c672bece
Prograwiz <73030056+Prograwiz@users.noreply.github.com>
2020-10-26 12:02:36 +0100

    Update Program.cs

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -82,7 +82,7 @@ namespace ConsoleApp
 
                         // 5 negative answers
                         "Don't count on it.",
-                        "My reply is no,",
+                        "My reply is no.",
                         "My sources say no.",
                         "Outlook not so good.",
                         "Very doubtful."
