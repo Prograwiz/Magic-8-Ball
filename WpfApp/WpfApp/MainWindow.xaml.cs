8234bdf26bdaf0f7f88fc8de56111693a7ea87ff
Prograwiz <prograwiz@protonmail.com>
2020-10-26 11:58:22 +0100

    Add WpfApp #1

---
+++ WpfApp/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitTheQuestion_OnClick(object sender, RoutedEventArgs e)
+        {
+            QuestionTextBox.Text = "";
+            GiveAnAnswer();
+
+            void GiveAnAnswer()
+            {
+                // Answers images paths
+                string[] answersPath =
+                {
+                    // 10 positive answers
+                    "pack://application:,,,/img/M8B_Positive01.png",
+                    "pack://application:,,,/img/M8B_Positive02.png",
+                    "pack://application:,,,/img/M8B_Positive03.png",
+                    "pack://application:,,,/img/M8B_Positive04.png",
+                    "pack://application:,,,/img/M8B_Positive05.png",
+                    "pack://application:,,,/img/M8B_Positive06.png",
+                    "pack://application:,,,/img/M8B_Positive07.png",
+                    "pack://application:,,,/img/M8B_Positive08.png",
+                    "pack://application:,,,/img/M8B_Positive09.png",
+                    "pack://application:,,,/img/M8B_Positive10.png",
+                    
+                    // 5 neutral answers
+                    "pack://application:,,,/img/M8B_Neutral01.png",
+                    "pack://application:,,,/img/M8B_Neutral02.png",
+                    "pack://application:,,,/img/M8B_Neutral03.png",
+                    "pack://application:,,,/img/M8B_Neutral04.png",
+                    "pack://application:,,,/img/M8B_Neutral05.png",
+                    
+                    // 5 negative answers
+                    "pack://application:,,,/img/M8B_Negative01.png",
+                    "pack://application:,,,/img/M8B_Negative02.png",
+                    "pack://application:,,,/img/M8B_Negative03.png",
+                    "pack://application:,,,/img/M8B_Negative04.png",
+                    "pack://application:,,,/img/M8B_Negative05.png",
+                };
+                
+                // Cast a random number
+                var rnd = new Random();
+                var answerIndex = rnd.Next(answersPath.Length);
+                var randomAnswer = answersPath[answerIndex];
+
+                AnswerDisplay.Source = new BitmapImage(new Uri(randomAnswer));
+            }
+        }
+    }
+}
